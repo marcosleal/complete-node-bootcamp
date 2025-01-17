de31e29ba0700a190e88daaba0b4585ac4169c01
Marcos Vinicius Lea da Silva <marcos.leal93@gmail.com>
2025-01-16 22:32:36 -0300

    feat: filling the templates

--- 1-node-farm-marcos/starter/index.js
+++ 1-node-farm-marcos/starter/index.js
@@ -31,6 +31,22 @@ const url = require('url');
 
 ////////////////////////////////////////////
 // SERVER
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
+    output = output.replace(/{%ID%}/g, product.id);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    }
+
+    return output;
+}   
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
@@ -45,18 +61,22 @@ const server = http.createServer((req, res) => {
     // Overview page
     if (pathName === '/' || pathName === '/overview') {
         res.writeHead(200, { 'Content-type': 'text/html' });
-        res.end(tempOverview);
 
-        // Product page
+        const cardsHtml = dataObject.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+        res.end(output);
+
+    // Product page
     } else if (pathName === '/product') {
         res.end('This is the PRODUCT!');
 
-        // API
+    // API
     } else if (pathName === '/api') {
         res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(data);
 
-        // Not found
+    // Not found
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
